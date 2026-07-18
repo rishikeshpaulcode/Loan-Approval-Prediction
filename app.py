610267845a99a1407926caaee8008158b313cb45
rishikeshpaulcode <rishikeshpaul.code@gmail.com>
2026-07-18 21:50:24 +0530

    codebase setup

--- app.py
+++ app.py
@@ -1,1 +1,53 @@
 # this module contains streamlit code for app interface
+import streamlit as st
+
+# create custom styles
+st.markdown(
+    '''
+    <style>
+        div.stButton > button p {
+            font-size: 21px !important;
+            forn-weight: bold !important;
+        }
+    </style>
+    ''',
+    unsafe_allow_html=True
+)
+st.markdown(
+    '''
+    <style>
+        div[data-testid="stSelectbox"] label[data-testid="stWidgetLabel"] p {
+            font-size: 22px !important;
+            font-weight: bold !important;
+        }
+    </style>
+    ''',
+    unsafe_allow_html=True
+)
+st.markdown(
+    '''
+    <style>
+        div[data-testid="stNumberInput"] label[data-testid="stWidgetLabel"] p {
+            font-size: 22px !important;
+            font-weight: bold !important;
+        }
+    </style>
+    ''',
+    unsafe_allow_html=True
+)
+st.markdown(
+    '''
+    <style>
+        .st-key-Pill1 label[data-testid="stWidgetLabel"] p,
+        .st-key-Pill2 label[data-testid="stWidgetLabel"] p {
+            font-size: 22px !important;
+            font-weight: bold !important;
+        }
+    </style>
+    ''',
+    unsafe_allow_html=True
+)
+
+
+if __name__ == "__main__":
+    pass
--- loan_approval_model/model_interface.py
+++ loan_approval_model/model_interface.py
@@ -1,1 +1,34 @@
 # use this module to interact with loan_approval_predictor model
+import numpy as np
+import pickle
+import json
+
+# load category codes map
+with open('category_codes.json', 'r') as file:
+    category_codes = json.loads(file)
+
+# load model
+with open('loan_approval_predictor.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+# function to encode features to modle understandable format
+def to_num(features):
+    # return dummy value
+    return list(range(13))
+
+
+# fuction to get predicted probabilites for each class
+def custom_pridict_proba(features):
+    # return dummy value
+    return [0.8, 0.2]
+
+
+# function to get predicted class
+def custom_predict(features):
+    # return dummy value
+    return 0
+
+
+# function to get features contributing to loan rejection
+def get_contri_features():
+    pass
